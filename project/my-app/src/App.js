e4c3475c245cea9e01d2bbfb327b5da33ac09dfa
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-06-03 16:19:58 +0900

    installed react

--- project/my-app/src/App.js
+++ project/my-app/src/App.js
@@ -1,19 +1,25 @@
-import React from 'react'; //Reactを読み込んでいる
-//画面遷移で使用する{ BrowserRouter, Route, Switch }を'react-router-dom'から読み込んでいる
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import page1 from './index'; //作成したpage1.jsを読み込んでいる
+import logo from './logo.svg';
+import './App.css';
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={page1} />
-        </Switch>
-      </BrowserRouter>
-    );
-  };
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
